0236b284c0123573a43c89dc7376490bf90a4f8c
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-10-08 21:56:09 +0300

    Add project files.

---
+++ TravelProject/TravelContext/TravelContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TravelProject.Models;
+
+namespace TravelProject.TravelContext
+{
+    public class TravelContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations  { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
